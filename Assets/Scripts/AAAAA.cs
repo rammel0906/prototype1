88c071b1402c18e6d76a7c114a78544863aecf6f
rammel0906 <kokita0208@gmail.com>
2026-01-01 00:32:22 +0900

    AAAAA.cs を更新

--- Assets/Scripts/AAAAA.cs
+++ Assets/Scripts/AAAAA.cs
@@ -5,7 +5,7 @@ public class AAAAA : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+		Debug.Log("aaaaa");
     }
 
     // Update is called once per frame
