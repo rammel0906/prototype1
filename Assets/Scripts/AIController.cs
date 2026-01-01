5bdebfe997c0787d7dba1ab043b67bbc98052ec8
rammel0906 <kokita0208@gmail.com>
2026-01-01 09:03:37 +0900

    Implement AI restart coroutine
    Added coroutine to restart AI and handle vehicle positions.

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -267,8 +267,39 @@ public class AIController : MonoBehaviour
         {
             gameObject.SetActive(true);
             isReset = false;
+            Startcoroutine(restartAI);
         }
-        InitStart();
+        else InitStart();
+    }
+
+    IEnumerator restartAI
+    {
+        Vector2 playerPos = new Vector2(transform.position.x, transform.position.z);
+        Vector2[] playerAxes = new Vector2[2];
+        playerAxes[0] = new Vector2(transform.right.x, transform.right.z);
+        playerAxes[1] = new Vector2(transform.forward.x, transform.forward.z);
+        float playerAngle = transform.eulerAngles.y;
+
+        foreach(var v in vehicles)
+        {
+            Vector2 vehiclePos = new Vector2(newVehicle.transform.position.x, newVehicle.transform.position.z);
+            Vector2[] vehicleAxes = new Vector2[2];
+            vehicleAxes[0] = new Vector2(newVehicle.transform.right.x, newVehicle.transform.right.z);
+            vehicleAxes[1] = new Vector2(newVehicle.transform.forward.x, newVehicle.transform.forward.z);
+        }
+
+        CR = new CalculationResult
+        (playerPos, playerAxes, playerAngle, vehiclePos, vehicleAxes);
+
+        FCIInfo.Clear();
+
+        rb.isKinematic = true;
+        isBreak = false;
+        isStop = false;
+
+        CalculationCourse();
+
+        StartCoroutine(PlayerMovement());
     }
 
     private void CalculationCourse()
